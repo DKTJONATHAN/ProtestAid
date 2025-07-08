c5a971c819075e11bb68f3be61f348252e1c92b1
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-07-09 01:47:14 +0300

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,33 +1,311 @@
 // Emergency responders data
 window.emergencyVolunteers = [
     {
-        "name": "Emergency Medical Responder",
+        "name": "Kenya Red Cross Nairobi",
         "county": "Nairobi",
-        "phone": "+254700XXXXXX",
+        "phone": "1199",
         "service": "medic",
-        "availability": "full-time",
-        "message": "Basic first aid available"
+        "availability": "on-call"
     },
     {
-        "name": "Legal Aid Volunteer",
+        "name": "St. John Ambulance Mombasa",
+        "county": "Mombasa",
+        "phone": "0722600600",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kisumu Fire Brigade",
+        "county": "Kisumu",
+        "phone": "0202222181",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Nakuru Red Cross",
+        "county": "Nakuru",
+        "phone": "1199",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "AAR Healthcare Eldoret",
+        "county": "Uasin Gishu",
+        "phone": "0725225225",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Nyeri Fire Brigade",
+        "county": "Nyeri",
+        "phone": "0202222181",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kenya Red Cross Nyeri",
+        "county": "Nyeri",
+        "phone": "1199",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "AMREF Flying Doctors",
+        "county": "Garissa",
+        "phone": "0206000552",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kakamega Fire Brigade",
+        "county": "Kakamega",
+        "phone": "0202222181",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Jonathan Mwaniki",
+        "county": "Kakamega",
+        "phone": "0706396305",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "JONATHAN MMWANIKI",
+        "county": "Meru",
+        "phone": "0706396305",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kitui Red Cross",
+        "county": "Kitui",
+        "phone": "1199",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "OCOA Ambulance Services",
+        "county": "Machakos",
+        "phone": "0758692000",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kilifi Fire Brigade",
+        "county": "Kilifi",
+        "phone": "0202222181",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kenya Red Cross Kilifi",
+        "county": "Kilifi",
+        "phone": "1199",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "AMREF Flying Doctors",
+        "county": "Tana River",
+        "phone": "0206000552",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Lamu Fire Brigade",
+        "county": "Lamu",
+        "phone": "0202222181",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kenya Red Cross Lamu",
+        "county": "Lamu",
+        "phone": "1199",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "St. John Ambulance",
+        "county": "Taita Taveta",
+        "phone": "0722600600",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Wajir Fire Brigade",
+        "county": "Wajir",
+        "phone": "0202222181",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kenya Red Cross Wajir",
+        "county": "Wajir",
+        "phone": "1199",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "AMREF Flying Doctors",
+        "county": "Mandera",
+        "phone": "0206000552",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Marsabit Fire Brigade",
+        "county": "Marsabit",
+        "phone": "0202222181",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kenya Red Cross Marsabit",
+        "county": "Marsabit",
+        "phone": "1199",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "St. John Ambulance",
+        "county": "Isiolo",
+        "phone": "0722600600",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Embu Fire Brigade",
+        "county": "Embu",
+        "phone": "0202222181",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kenya Red Cross Embu",
+        "county": "Embu",
+        "phone": "1199",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "AMREF Flying Doctors",
+        "county": "Kirinyaga",
+        "phone": "0206000552",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Murang'a Fire Brigade",
+        "county": "Murang'a",
+        "phone": "0202222181",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kenya Red Cross Murang'a",
+        "county": "Murang'a",
+        "phone": "1199",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "St. John Ambulance",
+        "county": "Kiambu",
+        "phone": "0722600600",
+        "service": "medic",
+        "availability": "on-call"
+    },
+    {
+        "name": "Law Society of Kenya",
         "county": "Nairobi",
-        "phone": "+254711XXXXXX", 
+        "phone": "0800720434",
         "service": "legal",
         "availability": "on-call"
     },
     {
-        "name": "Crisis Counselor",
-        "county": "Mombasa",
-        "phone": "+254722XXXXXX",
-        "service": "counseling",
-        "availability": "part-time"
+        "name": "Gikera & Vadgama Advocates",
+        "county": "Kisumu",
+        "phone": "0201234567",
+        "service": "legal",
+        "availability": "on-call"
     },
-    // Additional volunteers...
     {
-        "name": "Paramedic",
-        "county": "Eldoret",
-        "phone": "+254766XXXXXX",
-        "service": "medic",
-        "availability": "full-time"
+        "name": "Chepchieng & Company Advocates",
+        "county": "Nakuru",
+        "phone": "0712345678",
+        "service": "legal",
+        "availability": "on-call"
+    },
+    {
+        "name": "Wacu Mureithi & Co. Advocates",
+        "county": "Kitui",
+        "phone": "0712345679",
+        "service": "legal",
+        "availability": "on-call"
+    },
+    {
+        "name": "Mombasa Police Station",
+        "county": "Mombasa",
+        "phone": "999",
+        "service": "police",
+        "availability": "on-call"
+    },
+    {
+        "name": "Eldoret Police Station",
+        "county": "Uasin Gishu",
+        "phone": "999",
+        "service": "police",
+        "availability": "on-call"
+    },
+    {
+        "name": "Garissa Police Station",
+        "county": "Garissa",
+        "phone": "999",
+        "service": "police",
+        "availability": "on-call"
+    },
+    {
+        "name": "Tana River Police Station",
+        "county": "Tana River",
+        "phone": "999",
+        "service": "police",
+        "availability": "on-call"
+    },
+    {
+        "name": "Taita Taveta Police Station",
+        "county": "Taita Taveta",
+        "phone": "999",
+        "service": "police",
+        "availability": "on-call"
+    },
+    {
+        "name": "Mandera Police Station",
+        "county": "Mandera",
+        "phone": "999",
+        "service": "police",
+        "availability": "on-call"
+    },
+    {
+        "name": "Isiolo Police Station",
+        "county": "Isiolo",
+        "phone": "999",
+        "service": "police",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kirinyaga Police Station",
+        "county": "Kirinyaga",
+        "phone": "999",
+        "service": "police",
+        "availability": "on-call"
+    },
+    {
+        "name": "Kiambu Police Station",
+        "county": "Kiambu",
+        "phone": "999",
+        "service": "police",
+        "availability": "on-call"
     }
 ];
